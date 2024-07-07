68667d23d1a892c9f8433f8494ac29c495c86303
ken314526 <abhisheksharma801831@gmail.com>
2024-07-07 18:40:13 +0530

    Commit-7

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,65 +1,14 @@
 "use client";
 import React from "react";
-import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
-import aboutData from "@/data/about.json";
+import Education from "./Education";
+import Internship from "./Internship";
 
 export default function About() {
   return (
     <div className="pt-20" id="about">
       <p className="text-4xl text-center font-bold">About</p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {aboutData.about.map((data) => (
-          <CardContainer key={data.board} className="inter-var">
-            <CardBody className="relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-gray-700  w-full h-64 flex flex-col items-start justify-center sm:w-[30rem] sm:h-[18rem] rounded-xl p-6">
-              <CardItem
-                translateZ="50"
-                className="text-xl font-bold text-white dark:text-neutral-500"
-              >
-                {data.degree}
-              </CardItem>
-              <div className="flex flex-row items-center w-full justify-between">
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-white text-sm max-w-sm mt-2 dark:text-neutral-500"
-                >
-                  {data.board}
-                </CardItem>
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-white text-sm max-w-sm mt-2 dark:text-neutral-500"
-                >
-                  {data.year}
-                </CardItem>
-              </div>
-              <CardItem
-                as="p"
-                translateZ="60"
-                className="text-white text-sm max-w-sm mt-2 dark:text-neutral-500"
-              >
-                Domain: {data.domain}
-              </CardItem>
-              <CardItem
-                as="p"
-                translateZ="60"
-                className="text-white text-sm max-w-sm mt-2 dark:text-neutral-500"
-              >
-                {data.from}
-              </CardItem>
-              {data?.percentage && (
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-white text-sm max-w-sm mt-2 dark:text-neutral-500"
-                >
-                  {data.percentage}
-                </CardItem>
-              )}
-            </CardBody>
-          </CardContainer>
-        ))}
-      </div>
+      <Internship />
+      <Education />
     </div>
   );
 }
---
+++ src/components/Internship.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import Link from "next/link";
+import experienceData from "@/data/experience.json";
+
+export default function Internship() {
+  return (
+    <div className="pt-20">
+      <p className="text-4xl text-center font-bold">Internship</p>
+      <Link href={"/experience"}>
+        <div className="md:mx-2 mx-2 gap-4">
+          {experienceData.internship.map((p) => (
+            <CardContainer
+              key={p.id}
+              className="inter-var w-full h-auto sm:w-[30rem] sm:h-[24rem]"
+            >
+              <CardBody className="flex flex-col justify-center items-center dark:bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] w-full h-full rounded-xl p-6">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold dark:text-neutral-600 text-white text-center"
+                >
+                  {p.role}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="dark:text-neutral-500 text-sm max-w-sm mt-2 text-neutral-300 text-center"
+                >
+                  Company - {p.company}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="dark:text-neutral-500 text-sm max-w-sm mt-2 text-neutral-300 text-center"
+                >
+                  Description - {p.description}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="dark:text-neutral-500 text-sm max-w-sm mt-2 text-neutral-300 text-center"
+                >
+                  Duration - {p.duration}
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as={Link}
+                  href={p.link}
+                  target="_blank"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white mt-4"
+                >
+                  Go →
+                </CardItem>
+              </CardBody>
+            </CardContainer>
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
+}
